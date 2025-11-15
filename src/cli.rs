2c073598adb55fa664aec5604b0be046ff658e3e
lilyud69420 <duycvvn3@gmail.com>
2025-11-15 08:59:45 +0700

    feat: list CLI

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,22 @@
-use clap::{Parser};
+use clap::Parser;
 
 #[derive(Parser)]
-#[command(name = "DeskForge", version = "1.0", about = "A launcher creation tool", override_usage = "deskforge [COMMANDS] [OPTIONS]")]
+#[command(
+    name = "DeskForge",
+    version = "1.0",
+    about = "A launcher creation tool",
+    override_usage = "deskforge [COMMANDS] [OPTIONS]"
+)]
 pub struct Cli {
     /// Create a new launcher
-    #[arg(short = 'n', long = "new", value_name = "FILE_NAME", num_args = 0..=1)]
+    #[arg(short = 'n', long = "new", value_name = "OPTIONAL: FILE_NAME", num_args = 0..=1)]
     pub new: Option<Option<String>>,
-    
+
     /// Edit an existing launcher
-    #[arg(short = 'e', long = "edit", value_name = "FILE_NAME", num_args= 0..=1)]
+    #[arg(short = 'e', long = "edit", value_name = "REQUIRED: FILE_NAME", num_args= 0..=1)]
     pub edit: Option<String>,
+
+    /// List all exisiting launcher
+    #[arg(short = 'l', long = "list")]
+    pub list: bool,
 }
